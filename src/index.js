06e3311e9119ac1e22238717f745e86acd266f65
Farid-Ziade <61830209@students.liu.edu.lb>
2026-07-28 03:45:25 +0300

    Show remaining todo count on card and render placeholder todo divs
    Removes the unused empty todo.js module, updates the card header/remaining
    count styling, and adds a first pass at rendering todo divs under each
    project (rendering logic still has a known bug when adding/removing divs).

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { todo } from "./logic and dom/todo/todo.js";
+
 import { card } from "./logic and dom/todo/todoView.js";
 import { projectBar } from "./logic and dom/project/projectView.js";
 let container = document.createElement("div");
